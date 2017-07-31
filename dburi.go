4e580b4f7266c2769ebfb0ae4622ae369d6391e6
greg-sk <grzegorz.luczywo@securitykiss.com>
2017-07-31 10:36:35 +0100

    work in progress

--- dburi.go
+++ dburi.go
@@ -102,3 +102,151 @@ func (dbUri *DbUri) PgDumpSchema() (out string, err error) {
 	}
 	return string(b), nil
 }
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Generic DB functions
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// Generic DB String
+////////////////////////////////////////////////////////////////////////////////
+
+func GetStringRows(db *sql.DB, query string, args ...interface{}) ([][]string, error) {
+	var res [][]string
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	colNames, err := rows.Columns()
+	for rows.Next() {
+		//see http://stackoverflow.com/questions/14477941/read-select-columns-into-string-in-go
+		//why do I have to fight with the type system again?
+		readCols := make([]interface{}, len(colNames))
+		writeCols := make([]string, len(colNames))
+		for i, _ := range writeCols {
+			readCols[i] = &writeCols[i]
+		}
+		if err := rows.Scan(readCols...); err != nil {
+			return nil, err
+		}
+		res = append(res, writeCols)
+	}
+	return res, nil
+}
+
+func GetStringRow(db *sql.DB, query string, args ...interface{}) ([]string, error) {
+	ss, err := GetStringRows(db, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	if len(ss) == 0 {
+		return []string{}, nil
+	} else if len(ss) == 1 {
+		return ss[0], nil
+	} else {
+		return nil, fmt.Errorf("GetStringRow error: fetched %d results, expected 0 or 1", len(ss))
+	}
+}
+
+func GetStringColumn(db *sql.DB, query string, args ...interface{}) ([]string, error) {
+	ss, err := GetStringRows(db, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	if len(ss) == 0 {
+		return []string{}, nil
+	} else if len(ss[0]) != 1 {
+		return nil, fmt.Errorf("GetColumn error: query returns %d columns, expected 1", len(ss[0]))
+	} else {
+		var res []string
+		for _, row := range ss {
+			res = append(res, row[0])
+		}
+		return res, nil
+	}
+}
+
+func GetStringValue(db *sql.DB, query string, args ...interface{}) (string, error) {
+	s, err := GetStringRow(db, query, args...)
+	if err != nil {
+		return "", err
+	}
+	if len(s) != 1 {
+		return "", fmt.Errorf("GetValue error: query returned %d items, expected 1", len(s))
+	}
+	return s[0], nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Generic DB Int
+////////////////////////////////////////////////////////////////////////////////
+
+func GetIntRows(db *sql.DB, query string, args ...interface{}) ([][]int, error) {
+	var res [][]int
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	colNames, err := rows.Columns()
+	for rows.Next() {
+		//see http://stackoverflow.com/questions/14477941/read-select-columns-into-string-in-go
+		//why do I have to fight with the type system again?
+		readCols := make([]interface{}, len(colNames))
+		writeCols := make([]int, len(colNames))
+		for i, _ := range writeCols {
+			readCols[i] = &writeCols[i]
+		}
+		if err := rows.Scan(readCols...); err != nil {
+			return nil, err
+		}
+		res = append(res, writeCols)
+	}
+	return res, nil
+}
+
+func GetIntRow(db *sql.DB, query string, args ...interface{}) ([]int, error) {
+	ss, err := GetIntRows(db, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	if len(ss) == 0 {
+		return []int{}, nil
+	} else if len(ss) == 1 {
+		return ss[0], nil
+	} else {
+		return nil, fmt.Errorf("GetIntRow error: fetched %d results, expected 0 or 1", len(ss))
+	}
+}
+
+func GetIntColumn(db *sql.DB, query string, args ...interface{}) ([]int, error) {
+	ss, err := GetIntRows(db, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	if len(ss) == 0 {
+		return []int{}, nil
+	} else if len(ss[0]) != 1 {
+		return nil, fmt.Errorf("GetColumn error: query returns %d columns, expected 1", len(ss[0]))
+	} else {
+		var res []int
+		for _, row := range ss {
+			res = append(res, row[0])
+		}
+		return res, nil
+	}
+}
+
+func GetIntValue(db *sql.DB, query string, args ...interface{}) (int, error) {
+	s, err := GetIntRow(db, query, args...)
+	if err != nil {
+		return 0, err
+	}
+	if len(s) != 1 {
+		return 0, fmt.Errorf("GetValue error: query returned %d items, expected 1", len(s))
+	}
+	return s[0], nil
+}
